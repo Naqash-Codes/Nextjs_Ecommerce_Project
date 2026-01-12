b754bdce614e36842ffba7b1ab30d9a707f40fa4
Naqash-Codes <naqash.codes@gmail.com>
2026-01-12 18:28:11 +0500

    Show more Products

--- components/homepage/ExploreOurProducts.tsx
+++ components/homepage/ExploreOurProducts.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import Image from "next/image";
 import Heading from "../Heading";
 import { exploreProductList } from "@/data/products";
+import { useState } from "react";
 
 const ExploreOurProducts = () => {
+  const PRODUCTS_PER_ROW = 4;
+  const [visibleRows, setVisibleRows] = useState(2);
+  const visibleProducts = visibleRows * PRODUCTS_PER_ROW;
+
   return (
     <section className="px-6 xl:px-24 mt-10">
       <Heading title="Our Products" />
       <h1 className="text-5xl font-semibold mt-10">Explore Our Products</h1>
 
       <div className="mt-10 grid grid-cols-4 space-x-6 space-y-10">
-        {exploreProductList.map((product) => (
-          <div className="relative">
+        {exploreProductList.slice(0, visibleProducts).map((product) => (
+          <div key={product.id} className="relative">
             {/* Discount badge */}
             {product.disPer && (
               <div className="absolute top-3 left-3 bg-primary py-1 px-3 text-white text-xs rounded-md z-10">
@@ -51,6 +58,14 @@ const ExploreOurProducts = () => {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="mt-6 flex justify-center">
+        <button 
+          onClick={() => setVisibleRows((prev) => Math.min(prev + 1, Math.ceil(exploreProductList.length / PRODUCTS_PER_ROW)))}
+          className="bg-primary cursor-pointer px-8 py-4 text-white rounded-md ">
+          View All Products
+        </button>
       </div>
     </section>
   );
